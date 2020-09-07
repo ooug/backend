633cb12db5ce61a0296e02e01189552f4b358d56
avinashvidyarthi <avinashvidyaryhi@gmail.com>
2020-09-07 14:26:38 +0530

    added route to get contactUs requests and sending mail to while filling contactus form

--- src/services/app.service.ts
+++ src/services/app.service.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { contactUsModel, newsletterModel } from '../models/app';
+import { sendMail } from '../utils/mailer';
 
 // contact us
 export const contactUs = async (req: Request, res: Response) => {
@@ -13,17 +14,58 @@ export const contactUs = async (req: Request, res: Response) => {
   contactUs
     .save()
     .then(() => {
-      res.status(200).send({
-        status: true,
-        data: 'FORM_SUBMITTED',
-        path: req.path,
-        timestamp: Math.trunc(Date.now() / 1000),
-      });
+      sendMail(
+        req.body.email,
+        'Thank you for contacting OOUG.',
+        '',
+        'Dear ' +
+          req.body.name +
+          '<br><br>Thank you for contacting OOUG. Your request has been saved by us. Someone from our team will contact you soon.<br><br>Team OOUG'
+      )
+        .then(() => {
+          res.status(200).send({
+            status: true,
+            data: 'FORM_SUBMITTED',
+            path: req.path,
+            timestamp: Math.trunc(Date.now() / 1000),
+          });
+        })
+        .catch(() => {
+          res.status(200).send({
+            status: true,
+            data: 'FORM_SUBMITTED',
+            path: req.path,
+            timestamp: Math.trunc(Date.now() / 1000),
+          });
+        });
     })
     .catch(() => {
       res.send({
         status: false,
         data: 'ERROR',
+        path: req.path,
+        timestamp: Math.trunc(Date.now() / 1000),
+      });
+    });
+};
+
+// get contact us requests
+export const getContactUsRequests = async (req: Request, res: Response) => {
+  contactUsModel
+    .find()
+    .sort({ timeStamp: -1 })
+    .then((contactUsRequests) => {
+      res.status(200).send({
+        status: true,
+        data: contactUsRequests,
+        path: req.path,
+        timestamp: Math.trunc(Date.now() / 1000),
+      });
+    })
+    .catch((err) => {
+      res.status(200).send({
+        status: false,
+        data: err,
         path: req.path,
         timestamp: Math.trunc(Date.now() / 1000),
       });
@@ -72,12 +114,13 @@ export const newsletterSubscribe = async (req: Request, res: Response) => {
 };
 
 // newsletter unsubscribe
-export const newsletterUnsubscribe = async (req: Request, res: Response) =>{
-    // checking if already subscribed
-    newsletterModel.findOne({ email: req.body.email }).then((doc) => {
-      if (doc) {
-        // delete subscribed
-        newsletterModel.findOneAndDelete({email: req.body.email})
+export const newsletterUnsubscribe = async (req: Request, res: Response) => {
+  // checking if already subscribed
+  newsletterModel.findOne({ email: req.body.email }).then((doc) => {
+    if (doc) {
+      // delete subscribed
+      newsletterModel
+        .findOneAndDelete({ email: req.body.email })
         .then(() => {
           res.status(200).send({
             status: true,
@@ -95,16 +138,14 @@ export const newsletterUnsubscribe = async (req: Request, res: Response) =>{
             timestamp: Math.trunc(Date.now() / 1000),
           });
         });
-      } else
-      {
-        //if not subscribed
-        res.status(200).send({
-          status: false,
-          data: 'EMAIL_IS_NOT_SUBSCRIBED',
-          path: req.path,
-          timestamp: Math.trunc(Date.now() / 1000),
-        });
-      };
-    })
-
+    } else {
+      //if not subscribed
+      res.status(200).send({
+        status: false,
+        data: 'EMAIL_IS_NOT_SUBSCRIBED',
+        path: req.path,
+        timestamp: Math.trunc(Date.now() / 1000),
+      });
+    }
+  });
 };
