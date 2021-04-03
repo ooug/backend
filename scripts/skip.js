fe0db5db6239286d2266fbd9d75b755ebee0284e
SANTOSH MAHTO <mahtos480@gmail.com>
2021-04-03 16:02:37 +0530

    testing ci

--- scripts/skip.js
+++ scripts/skip.js
@@ -1,4 +1,8 @@
-console.info('--> Works --->>>>>>', process.env.INIT_CWD)
+/**
+ * In google cloud build we got INIT_CWD environment variable.
+ * But in case of local machine we not get this.
+ * Based on this key i will disable the husky installation on google cloud build
+ */
 if (process.env.INIT_CWD === '/workspace') {
   process.exit(0)
 } else {
