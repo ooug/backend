87eb0be8769ad6aaeeff0ce04289b9bcc224e965
SANTOSH MAHTO <mahtos480@gmail.com>
2021-04-03 15:53:17 +0530

    working with ci

--- scripts/skip.js
+++ scripts/skip.js
@@ -1,5 +1,5 @@
-console.info(process.env)
-if (process.env.BUILD_SKIP) {
+console.info('--> Works --->>>>>>', process.env.INIT_CWD)
+if (process.env.INIT_CWD === '/workspace') {
   process.exit(0)
 } else {
   process.exit(1)
