c0863c58da5f210116cebfdab4a246896f57d2a8
SANTOSH MAHTO <mahtos480@gmail.com>
2021-04-27 20:23:59 +0530

    Update index.js

--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 const { describe } = require('mocha')
 const { strictEqual } = require('assert')
-const { sum } = require('./utils/arithmetic')
+const { sum } = require('../utils/arithmetic')
 
 describe('arithmetic functions should work', () => {
   strictEqual(sum(2, 3), 5, 'fail summation')
